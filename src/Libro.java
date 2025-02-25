f78af1a4a19268986659a4121783398c2007c87b
Jllobera7 <jlloberavazquez@cifpfbmoll.eu>
2025-02-25 16:31:51 +0100

    nose

---
+++ src/Libro.java
@@ -0,0 +1,16 @@
+public class Libro {
+    //Atributos
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private int copiasTotales;
+    private int copiasDisponibles;
+
+
+
+
+
+
+
+}
